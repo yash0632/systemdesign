a88fb7089ba55ddf38cd588e67e67064444edd89
yash0632 <yashsharma0632@gmail.com>
2025-02-10 22:31:12 +0530

    Updated_package_structure

--- lldsysdesign/solid/PricingCalculator.ts
+++
@@ -1,7 +0,0 @@
-import { Product } from "./order";
-
-export class PricingCalculator {
-    calculatePricing(products:Product[]){
-        return products.reduce((total,product)=>total + product.price,0);
-    }
-}
---
+++ lldsysdesign/solid/srp/PricingCalculator.ts
@@ -0,0 +1,7 @@
+import { Product } from "./order";
+
+export class PricingCalculator {
+  calculatePricing(products: Product[]) {
+    return products.reduce((total, product) => total + product.price, 0);
+  }
+}
